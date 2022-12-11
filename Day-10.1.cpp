49722f3c343088c3ef9c9f6fbb12d5f2b1e9f9b8
anurag tiwari <anuragtiwari.ac@gmail.com>
2022-12-11 12:30:44 +0530

    this is a Day 10

---
+++ Day-10.1.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+    public:
+        vector <int> search(string pat, string txt)
+        {
+            vector<int>ans;
+            int n=txt.size(),m=pat.size();
+            for(int i=0;i<n;i++)
+            {
+                string temp=txt.substr(i,m);
+                if(temp==pat)
+                {
+                    ans.push_back(i+1);
+                }
+            }
+            if(ans.empty())
+                return {-1};
+            else
+                return ans;
+        }
+
+};
+int main()
+{
+    int t;
+    cout<<"enter the test case value"<<endl;
+    cin >> t;
+    while (t--)
+    {
+        string S, pat;
+        cout<<"enter the first input string"<<endl;
+        cin >> S ;
+        cout<<"enter  the second input string"<<endl;
+        cin>>pat;
+        Solution ob;
+        vector <int> res = ob.search(pat, S);
+        for (int i : res) cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
