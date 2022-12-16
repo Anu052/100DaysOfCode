2d54888854ca0a9eba950f2ff9ed7f2e8e083916
anurag tiwari <anuragtiwari.ac@gmail.com>
2022-12-16 16:27:11 +0530

    this is a day 15

---
+++ Day-15..1.cpp
@@ -0,0 +1,50 @@
+// Product of Array Except Self
+#include<bits/stdc++.h>
+using namespace std;
+class solve
+{
+    public:
+        vector<int> func(vector<int>a,int n)
+        {
+           vector<int>l(n),r(n);
+           l[0]=1;
+           r[n-1]=1;
+            for(int i=1;i<n;i++)
+            {
+                l[i]=l[i-1]*a[i-1];
+            }
+            for(int j=n-2;j>=0;j--)
+            {
+                r[j]=r[j+1]*a[j+1];
+            }
+            for(int i=0;i<n;i++)
+            {
+                a[i]=l[i]*r[i];
+            }
+            return a;
+        }
+};
+int main()
+{
+    int t;
+    cout<<"enter the testcase value"<<endl;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cout<<"enter the size of array"<<endl;
+        cin>>n;
+        vector<int>a(n);
+        cout<<"enter the array element"<<endl;
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        solve ob;
+        vector<int>ans=ob.func(a,n);
+        for(int i=0;i<ans.size();i++)
+        {
+            cout<<ans[i]<<endl;
+        }
+    }
+}
---
+++ Day-15.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+class solve
+{
+public:
+    bool func(int a[], int n, int k)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (abs(a[i] - a[j]) == k)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+int main()
+{
+    int t;
+    cout << "enter the test case value" << endl;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cout << "enter the size of array" << endl;
+        cin >> n;
+        int a[n];
+        cout << "enter the array element" << endl;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        int k;
+        cout << "enter the diffrence value" << endl;
+        cin >> k;
+        solve ob;
+        bool ans = ob.func(a, n, k);
+        if (ans == true)
+            cout << "yes pair is here" << endl;
+        else
+            cout << "no pair is not here" << endl;
+    }
+}
