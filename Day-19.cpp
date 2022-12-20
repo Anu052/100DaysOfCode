76313c4b50621850da7c3cf01a0d3809f9930288
anurag tiwari <anuragtiwari.ac@gmail.com>
2022-12-20 16:16:51 +0530

    this is a day 19

---
+++ Day-19.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    struct node *next;
+} *first = NULL;
+void create(int a[], int n)
+{
+    struct node *t, *last;
+    first = new node;
+    first->data = a[0];
+    first->next = NULL;
+    last = first;
+    for (int i = 1; i < n; i++)
+    {
+        t = new node;
+        t->data = a[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+struct node *reverseList(struct node *head)
+{
+    node *curr = head;
+    node *pre = NULL;
+    while (curr)
+    {
+        node *next = curr->next;
+        curr->next = pre;
+        pre = curr;
+        curr = next;
+    }
+    head = pre;
+    return head;
+}
+void display(struct node *p)
+{
+    while (p != NULL)
+    {
+        cout << p->data << endl;
+        p = p->next;
+    }
+}
+int main()
+{
+    int t;
+    cout << "enter the test case value" << endl;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cout << "enter the array size" << endl;
+        cin >> n;
+        int a[n];
+        cout << "enter the array element" << endl;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        create(a, n);
+        display(first);
+        cout << "after reverse the linked list" << endl;
+        struct node *t11;
+        t11 = reverseList(first);
+        display(t11);
+    }
+}
