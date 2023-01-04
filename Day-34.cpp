5330731e350c2a622bac0ee36aa6cafe387e74f6
anurag tiwari <87122561+Anu052@users.noreply.github.com>
2023-01-04 18:46:42 +0530

    this is a day 34

---
+++ Day-34.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d", arr[i]);
+    printf("\n");
+}
+void quickSort(int arr[], int low, int high)
+{
+    // code here
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(arr[j], arr[i]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+int main()
+{
+    int arr[1000], n, T, i;
+    scanf("%d", &T);
+    while (T--)
+    {
+        scanf("%d", &n);
+        for (i = 0; i < n; i++)
+            scanf("%d", &arr[i]);
+        quickSort(arr, 0, n - 1);
+        printArray(arr, n);
+    }
+    return 0;
+}
