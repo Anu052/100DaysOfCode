eff00849aadf1343a6b7b854443975dba60a279d
anurag tiwari <87122561+Anu052@users.noreply.github.com>
2023-01-15 17:24:09 +0530

    this is a day 45

---
+++ Day-45.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+    struct node *next;
+} *first = NULL;
+void create(int a[], int n)
+{
+    struct node *t, *last;
+    first = new node;
+    first->data = a[0];
+    first->next = NULL;
+    last = first;
+    for (int i = 1; i < n; i++)
+    {
+        t = new node;
+        t->data = a[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+void inorder(struct node *root, vector<int> ans)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left, ans);
+    ans.push_back(root->data);
+    inorder(root->right, ans);
+}
+vector<int> merge(struct node *p1, struct node *p2)
+{
+    vector<int> v;
+    inorder(p1, v);
+    inorder(p2, v);
+    sort(v.begin(), v.end());
+    return v;
+}
+void display(struct node *p)
+{
+    while (p != NULL)
+    {
+        cout << p->data << endl;
+        p = p->next;
+    }
+}
+int main()
+{
+    int t;
+    cout << "enter the test case value" << endl;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cout << "enter the size of array" << endl;
+        cin >> n;
+        int a[n];
+        cout << "enter the array element" << endl;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        create(a, n);
+        display(first);
+    }
+}
