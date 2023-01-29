580b1b8ed5b2e0f87b5db7620a0c7c67fc4469e6
anurag tiwari <anurag.t@rocketlearning.org>
2023-01-29 19:44:06 +0530

    this is a day 59

---
+++ day-59.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+
+	public:
+
+	int perfectSum(int arr[], int n, int sum)
+	{
+        int mod=1e9+7;
+        int t[n+1][sum+1];
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<sum+1;j++)
+            {
+                if(i==0)
+                    t[i][j]=0;
+                if(j==0)
+                    t[i][j]=1;
+            }
+        }
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=0;j<sum+1;j++)
+            {
+                if(arr[i-1]<=j)
+                {
+                    t[i][j]=(t[i-1][j]+t[i-1][j-arr[i-1]])%mod;
+                }
+                else
+                {
+                    t[i][j]=t[i-1][j]%mod;
+                }
+            }
+        }
+        return t[n][sum]%mod;
+	}
+
+    int main()
+    {
+        int t;
+        cin>>t;
+        while(t--)
+        {
+            int n,sum;
+            cin>>n>>sum;
+            int a[n];
+            for(int i=0;i<n;i++)
+            {
+             cin>>a[i];
+            }
+        }
+        Solution ob;
+    }
+	  
+};
