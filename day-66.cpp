4a556c7a6d80103f109323846c3ffbafbb96ca9f
anurag tiwari <anurag.t@rocketlearning.org>
2023-02-05 19:09:58 +0530

    this is a day 66

---
+++ day-66.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int countMin(string str){
+    int n= str.size();
+    vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+    
+    for(int i=0; i<n;i++){
+        for(int j=0; j<n;j++){
+            if(str[i]==str[n-1-j])
+            dp[i+1][j+1] = 1+dp[i][j];
+            else
+            dp[i+1][j+1] = max(dp[i][j+1],dp[i+1][j]);
+        }
+    }
+    return n-dp[n][n];
+}
+int main()
+{
+    int t;
+    cout<<"enter the test case value"<<endl;
+    cin>>t;
+    while(t--)
+    {
+        int s;
+        cout<<"enter the input string"<<endl;
+        cin>>s;
+        countMin(s);
+    }
+}
